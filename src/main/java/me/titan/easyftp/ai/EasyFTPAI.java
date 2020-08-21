64604742d6a8107a0f4dd4449fa37517401f4d56
TitanDevX <sdt199999@gmail.com>
2020-08-21 15:16:48 +0300

    Changed the UI look and feel from java to the os's native.

--- src/main/java/me/titan/easyftp/ai/EasyFTPAI.java
+++ src/main/java/me/titan/easyftp/ai/EasyFTPAI.java
@@ -57,6 +57,17 @@ public class EasyFTPAI extends JPanel {
 		JFrame j = new JFrame("Easy FTP");
 		j.setContentPane(new EasyFTPAI(main));
 		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 
 		j.pack();
 		j.setLocationRelativeTo(null);
